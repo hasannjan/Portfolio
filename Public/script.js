c7b9c41047fe1672af37277ee742cd7799a88f57
hasannjan <hasanjan2000@gmail.com>
2021-06-25 22:56:55 +0500

    Portfolio

--- Public/script.js
+++ Public/script.js
@@ -6,6 +6,32 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
 
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+
+    });
+
+    //typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Developer", "Intern", "Student", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Developer", "Intern", "Student", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // toggle menu/navbar script
